0643e2f73f13f1fc16b5f9205f511ab4c2dac77f
Marcos Felipe <mfelipesp@gmail.com>
2022-09-01 11:26:09 -0300

    Initial Commit

---
+++ PostsRedux/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PostsRedux
+//
+//  Created by marcos.felipe.souza on 01/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PostsRedux/PostsReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsReduxApp.swift
+//  PostsRedux
+//
+//  Created by marcos.felipe.souza on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PostsReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
